e6144189deb65bcdc40b256319d97cd50b448a3e
Emile Aben <emileaben@gmail.com>
2023-04-03 10:46:01 +0200

    with date as argument now

--- analyse.py
+++ analyse.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python
 import json
+import sys
 
-with open("data.json", 'rt') as inf:
+date=sys.argv[1] # as YYYY-MM-DD
+
+
+with open(f"data.{date}.json", 'rt') as inf:
     d = json.load( inf )
 
-for cc in d['countries']:
-    hhi = 0
-    #cdata = d['countries'][cc]
-    #print(cc)
-    if type( d['countries'][cc]['apnic'] ) != list:
-        continue
-    for net in d['countries'][cc]['apnic']:
-        frac = net['percent']*0.01 # convert to fraction
-        hhi += frac**2
-    print(f"{cc} {hhi}")
+outf_name = f"eyeball-hhi.{date}.csv"
+with open(outf_name,'wt') as outf:
+    for cc in d['countries']:
+        hhi = 0
+        #cdata = d['countries'][cc]
+        #print(cc)
+        if type( d['countries'][cc]['apnic'] ) != list:
+            continue
+        for net in d['countries'][cc]['apnic']:
+            frac = net['percent']*0.01 # convert to fraction
+            hhi += frac**2
+        print(f"{cc},{hhi:.3f}", file=outf)
+
+print(f"output written to {outf_name}", file=sys.stderr)
--- down.sh
+++ down.sh
@@ -1,1 +1,4 @@
-curl https://sg-pub.ripe.net/petros/population_coverage/data_sources/data07032022_0000.json > data.json
+#curl https://sg-pub.ripe.net/petros/population_coverage/data_sources/data07032022_0000.json > data.json
+#### format is dataDDMMYYYY_0000.json (TODO: maybe standardise to iso YYYY-MM-DD format
+curl https://sg-pub.ripe.net/petros/population_coverage/data_sources/data01012022_0000.json > data.2022-01-01.json
+curl https://sg-pub.ripe.net/petros/population_coverage/data_sources/data01012023_0000.json > data.2023-01-01.json
